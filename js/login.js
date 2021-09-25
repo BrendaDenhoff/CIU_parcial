156b985fef0d842d0cb62e5d972323bb828a4427
Brenda Denhoff <brenda.denhoffstudiantes.unahur.edu.ar>
2021-09-25 19:09:51 -0300

    subiendo

---
+++ js/login.js
@@ -0,0 +1,42 @@
+var contador = 1;
+
+var cambiarImagen = function(){
+    //var contador = 1;
+    if(contador == 5) {
+        contador = 1;
+    } else {
+        contador++;
+    }
+    document.getElementById("img").src = `imagenes/Imagenes-noticias/${contador}.png`;
+};
+
+
+
+setInterval(cambiarImagen,2750);
+
+var verificar = function() {
+    var nombre = document.getElementById("nombre").value;
+    var dni = document.getElementById("dni").value;
+    var contrasenia = document.getElementById("contrasenia").value;
+
+    if(nombre === "" || dni === "" || contrasenia === "") {
+        alert("Todos los campos son obligatorios");
+        return false;
+    } else if (isNaN(dni)) {
+        alert("El DNI es solo números");
+        return false;
+    } else if (dni.length != 8) {
+        alert("El DNI debe contener ocho digitos");
+        return false;
+    } else if(contrasenia.length < 4) {
+        alert("La contraseña debe contener al menos 4 digitos");
+        return false;
+    }
+    window.location = "paginaPrincipal.html";
+};
+
+document.getElementById("ingresar").addEventListener("click", verificar);
+
+
+
+
---
+++ js/paginaPrincipal.js
@@ -0,0 +1,37 @@
+var nombre = prompt("!Hola! ¿Podria indicarme su nombre?");
+
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = `¡Bienvenido ${nombre}!`;
+
+var inicio = document.querySelector(".inicio")
+
+inicio.addEventListener("click", function() {
+    window.open('login.html', `_self`);
+})
+
+var vacunate = document.querySelector(".vacunate")
+
+vacunate.addEventListener("click", function(){
+    window.open(`https://www.buenosaires.gob.ar/coronavirus/vacunacion-covid-19/poblacion-general`); //, `_blank`
+})
+
+var boton = document.querySelector(".boton") 
+
+boton.addEventListener("click", function(){
+    window.open(`../login.html`, `_self`);
+})
+
+var tarjetas = document.querySelectorAll(".tarjeta")
+
+for(var i=0; i<tarjetas.length;i++) {
+    tarjetas[i].addEventListener("mouseover", function() {
+        this.style.background = "#D6DBDF";
+    })
+}
+
+for(var i=0; i<tarjetas.length;i++) {
+    tarjetas[i].addEventListener("mouseleave", function() {
+        this.style.background = "#EBEDEF";
+    })
+}
